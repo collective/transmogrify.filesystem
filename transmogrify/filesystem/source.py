405ecf49234fb83b95d0030bbd26d2ec5c0358f9
JeanMichel FRANCOIS <toutpt@gmail.com>
2012-05-30 14:12:41 +0200

    pep8

--- transmogrify/filesystem/source.py
+++ transmogrify/filesystem/source.py
@@ -1,4 +1,3 @@
-import os
 import os.path
 import csv
 import mimetypes
@@ -13,11 +12,12 @@ from collective.transmogrifier.utils import Matcher
 
 from OFS.Image import File
 
+
 class FilesystemSource(object):
     """Custom section which can read files, folders and and images from the
     filesystem.
     """
-    
+
     implements(ISection)
     classProvides(ISectionBlueprint)
 
@@ -26,68 +26,75 @@ class FilesystemSource(object):
         self.name = name
         self.options = options
         self.previous = previous
-        
-        self.directory  = resolvePackageReferenceOrFile(options['directory'])
-        self.metadata   = None
+
+        self.directory = resolvePackageReferenceOrFile(options['directory'])
+        self.metadata = None
         self.delimiter = None
         self.strict = False
-        
+
         if 'metadata' in options:
             self.metadata = resolvePackageReferenceOrFile(options['metadata'])
             self.delimiter = options.get('delimiter', ',')
             self.strict = options.get('strict', False)
-        
-        self.requireMetadata = options.get('require-metadata', 'false').lower() != 'false'
-        
+
+        self.requireMetadata = options.get('require-metadata',
+                                           'false').lower() != 'false'
+
         self.folderType = options.get('folder-type', 'Folder')
-        
-        self.fileType   = options.get('file-type', 'File')
-        self.imageType  = options.get('image-type', 'Image')
-        
-        self.fileField  = options.get('file-field', 'file')
+
+        self.fileType = options.get('file-type', 'File')
+        self.imageType = options.get('image-type', 'Image')
+
+        self.fileField = options.get('file-field', 'file')
         self.imageField = options.get('image-field', 'image')
-        
-        self.wrapData   = options.get('wrap-data', 'true').lower() == 'true'
-        self.defaultMimeType = options.get('default-mime-type', 'application/octet-stream')
-        
+
+        self.wrapData = options.get('wrap-data', 'true').lower() == 'true'
+        self.defaultMimeType = options.get('default-mime-type',
+                                           'application/octet-stream')
+
         ignored = options.get('ignored') or ''
         self.ignored = Matcher(*ignored.splitlines())
 
     def __iter__(self):
-        
+
         for item in self.previous:
             yield item
-        
+
         metadata = {}
-        
+
         if self.metadata:
             metadataFile = open(self.metadata, 'rb')
             reader = csv.DictReader(metadataFile, delimiter=self.delimiter)
 
             if reader.fieldnames is None:
                 raise ValueError("Metadata CSV is empty.")
-            if len(reader.fieldnames) is 1 and self.delimiter not in reader.fieldnames[0]:
-                raise ValueError("Metadata CSV does not use the specified delimiter: %s" %(self.delimiter))
+            if len(reader.fieldnames) is 1 and \
+                self.delimiter not in reader.fieldnames[0]:
+                msg = "Metadata CSV does not use the specified delimiter: %s"
+                raise ValueError(msg % self.delimiter)
             if 'path' not in reader.fieldnames:
-                raise ValueError("Metadata CSV file does not have a 'path' column.")
+                msg = "Metadata CSV file does not have a 'path' column."
+                raise ValueError(msg)
 
             if self.strict:
                 field_count = len(reader.fieldnames)
-            
+
             for row in reader:
-                
+
                 if self.strict and field_count != len(row):
-                    raise ValueError("Found a row in Metadata CSV that has a different count of fields \
-                        compared to first row: %s" %(row))
-                
+                    msg = "Found a row in Metadata CSV that has a different \
+                    count of fields compared to first row: %s"
+                    raise ValueError(msg % row)
+
                 path = row['path']
                 data = row.copy()
                 del data['path']
                 metadata[path] = data
-        
+
         if self.requireMetadata and not metadata:
-            raise ValueError("Metadata is required, but metadata file %s not given or empty" % self.metadata)
-        
+            m = "Metadata is required, but metadata file %s not given or empty"
+            raise ValueError(m % self.metadata)
+
         if not os.path.exists(self.directory):
             raise ValueError("Directory %s does not exist" % self.directory)
 
@@ -97,56 +104,64 @@ class FilesystemSource(object):
             for dirname in dirnames:
                 dirPath = os.path.join(dirpath, dirname)
                 zodbPath = self.getZODBPath(dirPath)
-                
+
                 if self.ignored(zodbPath)[1]:
                     continue
-                
+
                 _type = self.folderType
-                
+
                 item = {'_type': self.folderType,
                         '_path': zodbPath}
-                
+
                 if zodbPath in metadata:
                     item.update(metadata[zodbPath])
-                
+
                 yield item
-            
+
             # Then import files
+
             for filename in filenames:
                 filePath = os.path.join(dirpath, filename)
                 zodbPath = self.getZODBPath(filePath)
-                
+
                 if self.ignored(zodbPath)[1]:
                     continue
-                
-                if self.metadata and os.path.abspath(filePath) == os.path.abspath(self.metadata):
+
+                if self.metadata and \
+                   os.path.abspath(filePath) == os.path.abspath(self.metadata):
                     continue
-                
+
                 if self.requireMetadata and zodbPath not in metadata:
                     continue
 
-                if zodbPath in metadata and 'portal_type' in metadata[zodbPath] and \
-                   metadata[zodbPath]['portal_type'] in ['News Item', 'Document', 'Event']:
+                if zodbPath in metadata and \
+                  'portal_type' in metadata[zodbPath] and \
+                   metadata[zodbPath]['portal_type'] in ['News Item',
+                                                         'Document',
+                                                         'Event']:
                     # if portal_type is given in metadata.csv, use it!
                     _type = metadata[zodbPath]['portal_type']
 
                     mimeType = 'text/html'
                     fieldname = 'text'
                     wrapData = False
-                    #if the file is an image: use the image field of the news item, otherwise use the text field
+                    # if the file is an image: use the image field of the
+                    # news item, otherwise use the text field
                     if _type == 'News Item':
                         basename, extension = os.path.splitext(filename)
-                        if extension and extension.lower() in mimetypes.types_map and mimetypes.types_map[extension.lower()].startswith('image'):
+                        if extension and \
+                           extension.lower() in mimetypes.types_map and \
+                           mimetypes.types_map[extension.lower()].startswith('image'):
                             mimeType = mimetypes.types_map[extension.lower()]
-                            fieldname = self.imageField # the same of news
+                            fieldname = self.imageField  # the same of news
                             wrapData = self.wrapData
 
-                else:                
+                else:
                     # else make it File or Image
                     _type = self.fileType
-                    fieldname = self.fileField  
+                    fieldname = self.fileField
                     mimeType = self.defaultMimeType
-                                  
+
                     # Try to guess mime type and content type
                     basename, extension = os.path.splitext(filename)
                     if extension and extension.lower() in mimetypes.types_map:
@@ -154,8 +169,8 @@ class FilesystemSource(object):
                         if mimeType.startswith('image'):
                             _type = self.imageType
                             fieldname = self.imageField
-                        
-                # read in main content of this item        
+
+                # read in main content of this item
                 infile = open(filePath, 'rb')
                 if wrapData:
                     fileData = File(filename, filename, infile, mimeType)
@@ -163,17 +178,17 @@ class FilesystemSource(object):
                 else:
                     fileData = infile.read()
                 infile.close()
-                
+
                 item = {'_type': _type,
                         '_path': zodbPath,
                         '_mimetype': mimeType,
                         fieldname: fileData}
-                
+
                 if zodbPath in metadata:
                     item.update(metadata[zodbPath])
-                
+
                 yield item
-    
+
     def getZODBPath(self, filePath):
         zodbPath = filePath[len(self.directory):]
         if os.path.sep != '/':
